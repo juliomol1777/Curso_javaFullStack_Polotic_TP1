3206f2f6ce51eeaea0996f2081b48f997e15f17f
julio mol <juliomolina17@gmail.com>
2022-11-14 14:07:09 -0300

    subida unica

---
+++ src/logica/Molina_Julio_tpo1.java
@@ -0,0 +1,16 @@
+
+package logica;
+
+import interfazGrafica.AgendaGUI;
+
+
+public class Molina_Julio_tpo1 {
+
+    
+    public static void main(String[] args) {
+        AgendaGUI agenda = new AgendaGUI();
+        agenda.setVisible(true);
+        agenda.setLocationRelativeTo(null);
+    }
+    
+}
